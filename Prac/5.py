240dbbbd2090bfba800b96a97fdd42c129d34c98
Innox1040 <uemj2003@gmail.com>
2026-01-27 15:15:27 +0530

    update ap

--- Prac/5.py
+++ Prac/5.py
@@ -1,6 +1,6 @@
 a = int(input("Enter number: "))
 
-rem = a % 10
+rem = abs(a) % 10
 print(rem)
 
 # this is the last digit of the number
---
+++ Prac/6.py
@@ -0,0 +1,4 @@
+d = int(input("Enter d: "))
+n = int(input("Enter n: "))
+
+print((d - n) % 7)
