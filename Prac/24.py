10598651e6ec4b9c48bfa4fdc835df8f544a3237
Innox1040 <uemj2003@gmail.com>
2026-02-03 16:03:32 +0530

    update 24

--- Prac/24.py
+++ Prac/24.py
@@ -1,10 +1,18 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-res = max(num1, num2)
+# res = max(num1, num2)
 
-while (res <= num1 * num2):
-    if (res % num1 == 0 and res % num2 == 0):
-        print(f"The LCM of {num1} and {num2} is {res}")
-        break
-    res += 1
+# while (res <= num1 * num2):
+#     if (res % num1 == 0 and res % num2 == 0):
+#         print(f"The LCM of {num1} and {num2} is {res}")
+#         break
+#     res += 1
+
+
+
+import math
+
+gcd = math.gcd(num1, num2)
+lcm = (num1 * num2) // gcd
+print(f"The LCM of {num1} and {num2} is {lcm}")
