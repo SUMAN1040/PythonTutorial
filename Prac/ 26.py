e8b88152bac1b46789475d5436e2012c73fd6692
Innox1040 <uemj2003@gmail.com>
2026-02-03 19:38:57 +0530

    add 26

---
+++ Prac/ 26.py
@@ -0,0 +1,13 @@
+# Check for prime in python
+
+num = int(input("Enter a number: "))
+
+if(num <= 1):
+    print(f"{num} is neither prime nor composite.")
+else:
+    for i in range(2, num):
+        if(num % i) == 0:
+            print(f"{num} is not a prime number.")
+            break
+    else:
+        print(f"{num} is a prime number.")
