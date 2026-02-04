1afd287783f3445b51cc65d5b8652d5a92542669
Innox1040 <uemj2003@gmail.com>
2026-02-04 20:27:01 +0530

    update 32

---
+++ Prac/32.py
@@ -0,0 +1,13 @@
+def count_digit(n):
+    # Convert the number to a string
+    num = str(n)
+
+
+    # Return the length of the string (number of digits)
+    return len(num)
+
+
+n = 58964
+
+# Output: 5
+print(count_digit(n))
