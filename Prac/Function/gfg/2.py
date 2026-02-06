7d6d64593bce0910321d5afca78a0560e6d15ff0
Innox1040 <uemj2003@gmail.com>
2026-02-07 00:59:14 +0530

    update 2.py

--- Prac/Function/gfg/2.py
+++ Prac/Function/gfg/2.py
@@ -1,14 +1,20 @@
-def fun1():
-    print("Hello World")
-    fun2()
-    print("After fun2")
+# def fun1():
+#     print("Hello World")
+#     fun2()
+#     print("After fun2")
 
+# def fun2():
+#     print("Welcome to Geeks")
 
+# print("Before fun1")
+# fun1()
+# print("After fun1")
 
-def fun2():
-    print("Welcome to Geeks")
+def fun():
+    x = 5
+    y = 10
+    x = y
+    print(x, y)
 
-
-print("Before fun1")
-fun1()
-print("After fun1")
+fun()
+fun()
