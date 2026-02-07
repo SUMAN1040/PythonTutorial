86564c3ccd7aca7ac44ca3e71567716c5e0f13dd
Innox1040 <uemj2003@gmail.com>
2026-02-07 16:47:26 +0530

    KeyWord Arguments

---
+++ Prac/Function/gfg/4.py
@@ -0,0 +1,12 @@
+# def printItem(id, name, price):
+#     print("Id is", id)
+#     print("Name is", name)
+#     print("Price is", price)
+
+# printItem(101, "abc", 100)
+# print()
+# printItem(id=102, price=200, name="xyz")
+# print()
+# printItem(name="abc", price=200, id=102)
+
+
