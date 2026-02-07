c7e504b2451415c17da1e11e127b4f66f34a5c7b
Innox1040 <uemj2003@gmail.com>
2026-02-07 20:03:10 +0530

    Returning Multiple Values in Python

---
+++ Prac/Function/gfg/7.py
@@ -0,0 +1,51 @@
+# def fun():
+#     return "GeeksForGeeks", 20
+
+# s, x = fun()
+# print(s)
+# print(x)
+
+
+
+# from dataclasses import dataclass
+
+# @dataclass
+# class Student:
+#     name: str
+#     marks: int
+
+# def get_student():
+#     return Student("Alice", 85)
+
+# student = get_student()
+# print(student.name)  
+# print(student.marks)
+
+
+# def fun():
+#     d = {"str": "GeeksforGeeks", "x": 20}
+#     return d
+
+# d = fun()
+# print(d)
+
+
+#Using Object
+# class Info:
+#     def __init__(self):
+#         self.name = "Alice"
+#         self.age = 25
+
+# def get_info():
+#     return Info()
+
+# person = get_info()
+# print(person.name)
+# print(person.age)
+
+#Using List
+def fun():
+    l = ["GeeksForGeeks", 20]
+    return l
+d = fun()
+print(d)
