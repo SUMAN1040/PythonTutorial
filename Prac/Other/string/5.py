70676f6044656e5022206c0f4061803e8549cde8
Innox1040 <uemj2003@gmail.com>
2026-02-08 20:25:18 +0530

    String Operation Part 2

---
+++ Prac/Other/string/5.py
@@ -0,0 +1,43 @@
+# s1 = "Geeks For Geeks"
+# print(len(s1))
+
+# print(s1.upper())
+
+# print(s1.lower())
+
+# print(s1.islower())
+
+# print(s1.isupper())
+
+# print(s1.startswith("G"))
+
+# print(s1.endswith("s"))
+
+# print(s1.startswith("G", 1))
+
+# print(s1.startswith("Geeks", 2, len(s1)))
+
+# print(s1.split())
+
+# print(s1.split(", "))
+
+
+# l = ['Geeks', 'For', 'Geeks']
+# print(", ".join(l))
+
+
+# s = "---Geeks For Geeks---"
+
+# print(s.strip("-"));
+# print(s.lstrip("-"));
+# print(s.rstrip("-"))
+# print(s.strip(""))
+
+
+a1 = "Suman Kumar Dey"
+a2 = "Dey"
+
+print(a1.find(a2))
+print(a1.find("hello"))
+
+print(a1.find(a2, 1, len(a1)))
