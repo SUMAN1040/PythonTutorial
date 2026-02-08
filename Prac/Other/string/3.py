8c50c4aeea1d78758b8a9ef68a2312982c600d55
Innox1040 <uemj2003@gmail.com>
2026-02-08 20:00:09 +0530

    Formatted String in Python

---
+++ Prac/Other/string/3.py
@@ -0,0 +1,11 @@
+name = "Suman"
+course = "InnoX"
+
+s = "Welcome %s to the company %s" %(name, course)
+print(s)
+
+format = "welcome {0} to the {1} ".format(name, course)
+print(format)
+
+fString = f" welcome {name} to the {course}"
+print(fString)
