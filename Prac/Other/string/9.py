071843f8baff5efe78e108e822586f25bcbe56b5
Innox1040 <uemj2003@gmail.com>
2026-02-08 21:19:40 +0530

    Check Palindrome In Python

---
+++ Prac/Other/string/9.py
@@ -0,0 +1,21 @@
+# s = input()
+# low = 0
+# high = len(s) - 1
+
+# while low < high:
+#     if s[low] != s[high]:
+#         print("No")
+#         break
+#     low = low + 1
+#     high = high - 1
+# else:
+#     print("Yes")
+
+
+# Check Palindrome or Not
+# s = input("Enter a string: ")
+# if(s == s[::-1]):
+#     print("yes")
+# else:
+#     print("No")
+
