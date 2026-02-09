81ab6001709a811d0d4ab837ce37b5f96f09e5bd
Innox1040 <uemj2003@gmail.com>
2026-02-09 21:25:22 +0530

    update string

---
+++ Prac/Other/string/Str/2.py
@@ -0,0 +1,8 @@
+#String Comparison in python
+#Lexicographically
+
+#Using start with and ends with methods
+s = "SumanKumarDey"
+
+print(s.startswith("Suman"))
+print(s.endswith('Dey'))
---
+++ Prac/Other/string/Str/3.py
@@ -0,0 +1,8 @@
+txt = "geeks for geeks"
+pat = "geeks"
+
+pos = txt.find(pat)
+
+while pos >= 0:
+    print(pos)
+    pos = txt.find(pat, pos + 1)
