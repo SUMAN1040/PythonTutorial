17dacac1a76f247376ce9715b10da5de711c0f07
Innox1040 <uemj2003@gmail.com>
2026-02-09 15:25:39 +0530

    binary to decimal conversion function

---
+++ Prac/Other/string/10.py
@@ -0,0 +1,13 @@
+# def binToDec(b):
+#     res = 0
+#     p = 1
+#     for i in reversed(b):
+#         res = res + int(i)*p
+#         p = p * 2
+#     return res
+# print(binToDec('1100'))
+
+def binToDec(b):
+    res = int(b, 2)
+    return res
+print(binToDec('1100'))
