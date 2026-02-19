321dfdec6dd8ebed69aad90ebce755fd614f8114
Innox1040 <uemj2003@gmail.com>
2026-02-19 19:47:44 +0530

    Removed __pycache__ and added .gitignore

--- BackendWithDjango/Eco_Efficient/Eco_Efficient/urls.py
+++ BackendWithDjango/Eco_Efficient/Eco_Efficient/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Eco_Efficient.views import innox
+# from django.urls import 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
