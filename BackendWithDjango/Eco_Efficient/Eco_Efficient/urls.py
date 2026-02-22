b282d7ef4da378ac16e3576a9541bd71a5254c7b
Innox1040 <uemj2003@gmail.com>
2026-02-22 23:19:27 +0530

    add new django app

--- BackendWithDjango/Eco_Efficient/Eco_Efficient/urls.py
+++ BackendWithDjango/Eco_Efficient/Eco_Efficient/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Eco_Efficient.views import innox
-# from django.urls import 
+from ui.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', innox),
+    path('', include('ui.urls'))
 ]
