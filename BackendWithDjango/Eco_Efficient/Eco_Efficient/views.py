a221dc8198add85834e2cc62c784b682303983a4
Innox1040 <uemj2003@gmail.com>
2026-02-24 23:29:47 +0530

    Update all

--- BackendWithDjango/Eco_Efficient/Eco_Efficient/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def innox(request):
-    return HttpResponse("Hey! Suman, Welcome to this python backend Course")
---
+++ BackendWithDjango/Sahaayak/Sahaayak/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def womenSafetyApplication(request):
+    return HttpResponse("Hey Suman, Welcome you in this django learning path")
