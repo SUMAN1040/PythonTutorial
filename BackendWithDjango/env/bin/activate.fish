b192b18741e973132d895dec107aa01cb4c0e495
SUMAN1040 <uemj2003@gmail.com>
2026-05-19 10:10:01 +0530

    chore: update project dependencies and environment packages via pip</div>

--- BackendWithDjango/env/bin/activate.fish
+++ BackendWithDjango/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/InnoX107/Documents/PythonTutorial/BackendWithDjango/env"
+set -gx VIRTUAL_ENV "/home/InnoX107/Documents/Projects/PythonTutorial/BackendWithDjango/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
