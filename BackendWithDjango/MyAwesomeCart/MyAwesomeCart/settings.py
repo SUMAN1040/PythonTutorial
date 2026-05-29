01b9a4bde7e741cf53986275c1ae76414b31e067
SUMAN1040 <uemj2003@gmail.com>
2026-05-29 22:33:34 +0530

    style: remove extraneous whitespace from settings.py

--- BackendWithDjango/MyAwesomeCart/MyAwesomeCart/settings.py
+++ BackendWithDjango/MyAwesomeCart/MyAwesomeCart/settings.py
@@ -72,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'MyAwesomeCart.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -82,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -102,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -113,7 +110,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
