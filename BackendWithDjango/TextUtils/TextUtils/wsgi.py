734b08623f5899c8743a4b249d15a25f32ef3ca4
SUMAN1040 <uemj2003@gmail.com>
2026-07-04 17:37:29 +0530

    refactor: migrate Django project from Sahaayak to TextUtils directory structure

--- BackendWithDjango/TextUtils/TextUtils/wsgi.py
+++ BackendWithDjango/TextUtils/TextUtils/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Sahaayak project.
+WSGI config for TextUtils project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sahaayak.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TextUtils.settings')
 
 application = get_wsgi_application()
