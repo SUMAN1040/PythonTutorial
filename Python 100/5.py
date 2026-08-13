2f030b4b9fe86916700a84b29180e44db68880e1
SUMAN1040 <uemj2003@gmail.com>
2026-08-13 10:40:43 +0530

    feat: add exercise 5 demonstrating print function features and delimiters

---
+++ Python 100/5.py
@@ -0,0 +1,8 @@
+# Hey suman dont remove this line
+print("Hey I am a good boy \nand this views is also a god boy/girl")
+
+print("Hey Dude,\"What's up\"")
+
+print("Hey", 6, 7, sep = "~")
+print(end = " \n ")
+print("Suman")
