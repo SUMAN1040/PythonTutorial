ba2afc2e0d8a39c887026bf09d25f2689f7b46a1
SUMAN1040 <uemj2003@gmail.com>
2026-08-17 22:42:46 +0530

    feat: add script demonstrating basic variable types and type checking

---
+++ Python 100/6.py
@@ -0,0 +1,12 @@
+a = 1
+print(a)
+
+b = "Suman"
+print(b)
+
+#print(a + b) #If we wanna concatinate then both must be same type
+
+print("The type of a is: ", type(a))
+print("The type of b is: ", type(b))
+
+print(type(complex(3, 4)))
