f541106974779b077b33ece935ecd0d6bdee0bdf
SUMAN1040 <uemj2003@gmail.com>
2026-09-05 10:15:39 +0530

    feat: remove snake game file and expand Python 100 tutorials with new list, tuple, and dictionary examples

--- Python 100/6.py
+++ Python 100/6.py
@@ -10,3 +10,16 @@ print("The type of a is: ", type(a))
 print("The type of b is: ", type(b))
 
 print(type(complex(3, 4)))
+
+
+# List: List is an order collection of data with elements separated by a comma and enclosded within squre brackets
+list = [8, 2.3, [-2, 4], ["apple", "banana"]]
+print(list)
+
+#Tuple: A tuple is an order collection of data with elements separated by a comma and enclosded within parentheses
+tuple = (("Parrot", "Fish", "Cat"), ("Lion", "Dog", "Tiger"))
+print(tuple)
+
+#Dict: A Dictionary is an unorderd collection of data with elements separated by a comma and enclosded within curly brackets
+dict = {"Name": "Suman", "Age": 23, "Class": "4th Year B.Tech"}
+print(dict)
