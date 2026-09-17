5ea1d866455dd2675ff7e0eced47ba917b332eeb
SUMAN1040 <uemj2003@gmail.com>
2026-09-17 06:37:47 +0530

    feat: add script demonstrating explicit and implicit type casting

---
+++ Python 100/9.py
@@ -0,0 +1,17 @@
+#Explicite Type Casting
+string = "15"
+number = 7
+string_number = int(string)
+# Through an error if the string is not valid integer
+
+sum = number + string_number
+print("The Sum of both the numbers is:", sum)
+
+# String Concatenation 
+# Through an error if the string is not valid string
+
+#Implecite Type Casting
+a = 3.2
+b = 1
+
+print(a + b)
