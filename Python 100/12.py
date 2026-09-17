e508ff1137c1b3e0deff219e71dbe0892e2413a1
SUMAN1040 <uemj2003@gmail.com>
2026-09-17 13:17:37 +0530

    feat: add script demonstrating string slicing and negative indexing

---
+++ Python 100/12.py
@@ -0,0 +1,15 @@
+name = "Suman Kumar Dey"
+
+print(name[0:5])
+print(len(name))
+
+print(name[0 : 11])
+print(name[:])
+
+#Negative Slicing
+print(name[0 : len(name) - 3])
+
+
+#Quick Quiz
+nm = "Harry"
+print(nm[-4 : -2])
