679e6bbb21f0431c746fe22a22cd8778798e8f63
SUMAN1040 <uemj2003@gmail.com>
2026-09-17 12:35:50 +0530

    feat: add Python script demonstrating string operations and loops

---
+++ Python 100/11.py
@@ -0,0 +1,24 @@
+name = "Suman"
+friend = 'Kumar'
+
+print("My name is " + name + "\nand my friend name is " + friend)
+
+
+apple = '''
+Hi Suman!,
+How are you?
+how your engineering journey are going on?
+
+'''
+print(apple)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+print("\nlet's use the for loop\n")
+
+for character in name:
+    print(character)
